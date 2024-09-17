fbb279fc5a96ef2feff5ee18e7d886be1a0c543c
Carstens <jannis.carstens@btc-ag.com>
2024-09-17 13:55:44 +0200

    first commit

--- src/main/java/com/btcag/bootcamp/test.java
+++ src/main/java/com/btcag/bootcamp/test.java
@@ -1,4 +1,14 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte Zahl eingeben zur Quadratzahlberechnung");
+       long thenumber = scanner.nextLong();
+       thenumber *= thenumber;
+       System.out.println("Ergebnis:" + thenumber);
+    }
 }
