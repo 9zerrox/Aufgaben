01756834e602fa40056ec51f0e2303817ea7d202
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 08:56:39 +0200

    name changes

--- src/main/java/com/btcag/bootcamp/ArtihOp.java
+++ src/main/java/com/btcag/bootcamp/ArtihOp.java
@@ -1,8 +1,6 @@
 package com.btcag.bootcamp;
 
-import java.util.Scanner;
-
-public class aritmetische {
+public class ArtihOp {
     public static void main(String[] args) {
         int var = 10;
         System.out.println(++var);
---
+++ src/main/java/com/btcag/bootcamp/Backward.java
@@ -0,0 +1,14 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Backward {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Gebe eine positive ganze Zahl ein:");
+        int number = scanner.nextInt();
+
+        long
+    }
+}
