9c857499c9ef5a3bc67a59f6ad7b19ded06bef20
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 13:23:05 +0200

    finished task

---
+++ src/main/java/com/btcag/bootcamp/ASCIISquare.java
@@ -0,0 +1,22 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class ASCIISquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Groesse des Vierecks: ");
+        int number = scanner.nextInt();
+        int i = 0;
+        while(i < number){
+            int j = 0;
+            while (j < number){
+                System.out.print("*");
+                j++;
+            }
+            System.out.println("");
+            i++;
+        }
+
+    }
+}
