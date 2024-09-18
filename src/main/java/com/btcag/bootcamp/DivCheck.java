9e9133f9b9dc92856e4e7d04fac39c96e2669b2c
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 08:32:11 +0200

    Problem solved

--- src/main/java/com/btcag/bootcamp/DivCheck.java
+++ src/main/java/com/btcag/bootcamp/DivCheck.java
@@ -1,4 +1,22 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class DivCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bitte gib eine positive ganze Zahl ein:");
+        int divisor = scanner.nextInt();
+
+        System.out.println("Bitte gib eine positive ganze Zahl ein:");
+        int maxnumber = scanner.nextInt();
+
+        int result = 0;
+
+        while (result < maxnumber){
+            System.out.println(result);
+            result += divisor;
+        }
+    }
 }
