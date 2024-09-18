92a18fd69e18ce965353fc0f4d85677fb2c3f241
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 08:07:44 +0200

    new files

---
+++ src/main/java/com/btcag/bootcamp/circle.java
@@ -0,0 +1,15 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class circle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe eine positive ganze Zahl ein:");
+        int number = scanner.nextInt();
+        float pi = 3.14F;
+        float n = number*number;
+        float result = pi * n;
+        System.out.println("Der Kreis hat eine Fläche von" + result);
+    }
+}
---
+++ src/main/java/com/btcag/bootcamp/rectangular.java
@@ -0,0 +1,21 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class rectangular {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gebe eine positive ganze Zahl ein:");
+        int number = scanner.nextInt();
+
+        Scanner secondscan = new Scanner(System.in);
+        System.out.println("Gebe eine positive ganze Zahl ein:");
+        int nextnumber = secondscan.nextInt();
+
+        float result = number * nextnumber;
+
+        System.out.println("Das Ergebnis lautet" + result);
+
+        }
+    }
+
