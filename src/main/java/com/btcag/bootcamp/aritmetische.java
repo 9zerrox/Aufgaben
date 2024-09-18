f2f015f4bebca66f8f978d5c557a85bdbd62753e
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 08:42:15 +0200

    new solution

---
+++ src/main/java/com/btcag/bootcamp/aritmetische.java
@@ -0,0 +1,12 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class aritmetische {
+    public static void main(String[] args) {
+        int var = 10;
+        System.out.println(++var);
+        System.out.println(var++);
+        System.out.println(var);
+    }
+}
