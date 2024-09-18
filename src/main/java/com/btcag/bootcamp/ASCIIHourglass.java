77f36c20e6c12f9a19216e618e9cc36e3f25f7fe
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 15:38:23 +0200

    first commit

---
+++ src/main/java/com/btcag/bootcamp/ASCIIHourglass.java
@@ -0,0 +1,18 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class ASCIIHourglass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Groesse einer Sanduhr haelfte: ");
+        int number = scanner.nextInt();
+
+        int i = number;
+        while (i > 0){
+            System.out.print("*");
+            i--;
+            int j = 0;
+        }
+        }
+    }
