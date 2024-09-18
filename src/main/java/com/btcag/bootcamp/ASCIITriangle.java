10a598d1d3d21dc3244663f2399184a388b2b752
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 13:39:04 +0200

    finished task

---
+++ src/main/java/com/btcag/bootcamp/ASCIITriangle.java
@@ -0,0 +1,22 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class ASCIITriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gib eine ganze Zahl ein: ");
+        int number = scanner.nextInt();
+
+        int i = 0;
+        while (i <= number) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
