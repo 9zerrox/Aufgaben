ee44551d4b9b9b70a6dd051dd0ed4f69c7220a3d
Carstens <jannis.carstens@btc-ag.com>
2024-09-19 10:49:59 +0200

    first commit

---
+++ src/main/java/com/btcag/bootcamp/PrimeFactorization.java
@@ -0,0 +1,14 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class PrimeFactorization {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int i = 0;
+        while (i != n) {
+            int x = n/n
+        }
+    }
+}
