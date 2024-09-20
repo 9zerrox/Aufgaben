002419f0a5ccf307af7e973bbb98ae70b3935ff1
Carstens <jannis.carstens@btc-ag.com>
2024-09-20 13:12:22 +0200

    stuck

--- src/main/java/com/btcag/bootcamp/PrimeTest.java
+++ src/main/java/com/btcag/bootcamp/PrimeTest.java
@@ -2,11 +2,35 @@ package com.btcag.bootcamp;
 
 import java.util.Scanner;
 
+// add all the digits in a number, if the sum is divisible by 3 it is not a prime number //
+
 public class PrimeTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        String input = scanner.nextLine();
 
+        // Dynamische Variablen zur Speicherung der Ziffern
+        int length = input.length();
+        char[] digits = new char[length];
+
+        // Ziffern in das Array speichern
+        int i = 0;
+        while (i < length) {
+            digits[i] = input.charAt(i);
+            i++;
+        }
+
+        // Ausgabe der Ziffern in einzelnen Variablen
+        int j = 0;
+        while (j < length) {
+            System.out.println("char " + (j + 1) + " = " + digits[j]);
+            j++;
+        }
+
+        int n = 0;
+        while (n < 1){
+            int sum = char
+        }
 
     }
 }
