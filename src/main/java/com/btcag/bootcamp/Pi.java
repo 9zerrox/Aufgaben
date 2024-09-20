4baf340f986b3771bf0458027185548a0a20d023
Carstens <jannis.carstens@btc-ag.com>
2024-09-20 11:16:57 +0200

    pi more or less

---
+++ src/main/java/com/btcag/bootcamp/Pi.java
@@ -0,0 +1,23 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Pi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wie viele Iterationen sollen gemacht werden um Pi zu berechnen?");
+        int n = scanner.nextInt();
+        double pi = 0.0;
+        int i = 0;
+
+        while (i < n){
+            pi += Math.pow(-1, i) / (2 * i + 1);
+            i++;
+        }
+
+        pi *= 4;
+        System.out.println("Annäherung nach "+n+" Iterationen: "+pi);
+
+
+    }
+}
