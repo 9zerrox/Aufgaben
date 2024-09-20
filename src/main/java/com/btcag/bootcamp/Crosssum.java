59a5af43b0be5e95616996bbef897a412b71cb5b
Carstens <jannis.carstens@btc-ag.com>
2024-09-20 08:33:28 +0200

    finished task

--- src/main/java/com/btcag/bootcamp/Crosssum.java
+++ src/main/java/com/btcag/bootcamp/Crosssum.java
@@ -1,7 +1,18 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Crosssum {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte Zahl eingeben (Long): ");
+        int zahl = scanner.nextInt();
+        int result = 0;
+        while (zahl != 0) {
+            result += zahl % 10;
+            zahl /= 10;
+        }
+        System.out.println(result);
     }
 }
+
