9d2adeeeb901aec9329a31c53d1720c231a51c0c
Carstens <jannis.carstens@btc-ag.com>
2024-09-23 12:53:27 +0200

    working

--- src/main/java/com/btcag/bootcamp/Supermarket.java
+++ src/main/java/com/btcag/bootcamp/Supermarket.java
@@ -1,7 +1,17 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Supermarket {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte alle Produkte in Cent eingeben und mit 0 die Liste beenden:");
+        long input = 0;
+        long geld = 0;
+            do{
+                input = scanner.nextLong();
+                geld += input;
+            } while (input != 0);
+        }
     }
-}
+
