aebb0572a8cf9bf8a42db192a83f40477583196c
Carstens <jannis.carstens@btc-ag.com>
2024-09-25 14:08:12 +0200

    solution

---
+++ src/main/java/com/btcag/bootcamp/Schaltjahre.java
@@ -0,0 +1,18 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Schaltjahre {
+    public static void main(String[] args) {
+        while (1 == 1) {
+            Scanner readIn = new Scanner(System.in);
+            int year = readIn.nextInt();
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println("Es handelt sich um ein Schaltjahr!");
+            } else {
+                System.out.println("Es handelt sich um kein Schaltjahr!");
+            }
+        }
+    }
+}
